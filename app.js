dbc937974fbcf7087a73ecbcc62ab53674d4c221
Halley <h0000151515@gmail.com>
2022-03-16 19:51:05 +0700

    first change

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 
-const mongoUrl = 'mongodb://127.0.0.1:27017/myDb';
+const mongoUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/myDb';
 const port = process.env.PORT || 3000;
 
 //CORS
